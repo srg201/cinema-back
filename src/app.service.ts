2363c5c9f9a21d2e9b51bf6c4fe908f7ba511df8
srg201 <87131271+srg201@users.noreply.github.com>
2022-12-10 03:14:15 +0300

    Add files via upload

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common'
 @Injectable()
 export class AppService {
   getHello() {
-    return { text: 'Hello htmllessons.ru!' }
+    return 'Server is working!'
   }
 }
